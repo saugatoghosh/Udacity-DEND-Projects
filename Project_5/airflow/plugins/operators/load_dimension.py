8ef50e52106e972cf7c289a58dce3e95a0aa8271
Sougata Ghosh <saugato_ghosh@hotmail.com>
2019-07-31 18:36:32 +0530

    Update load_dimension.py

--- Project_5/airflow/plugins/operators/load_dimension.py
+++ Project_5/airflow/plugins/operators/load_dimension.py
@@ -2,6 +2,8 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+# Custom operator for loading dimension tables
+
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
@@ -28,6 +30,10 @@ class LoadDimensionOperator(BaseOperator):
         self.truncate=truncate
 
     def execute(self, context):
+        '''
+        Either empties and then loads dimension table or
+        simply loads dimension table
+        '''
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         if(self.truncate == True):
             self.log.info('Clearing data from target dimension table')
