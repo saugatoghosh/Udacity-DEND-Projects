52aa984a5527e98a1f1d9f759e03cc26b1d31b8d
Sougata Ghosh <saugato_ghosh@hotmail.com>
2019-07-31 18:38:20 +0530

    Update load_fact.py

--- Project_5/airflow/plugins/operators/load_fact.py
+++ Project_5/airflow/plugins/operators/load_fact.py
@@ -2,6 +2,8 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+#Loads the fact table from staging tables
+
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
@@ -24,6 +26,9 @@ class LoadFactOperator(BaseOperator):
         self.sql_query=sql_query
 
     def execute(self, context):
+        '''
+        Appends data to the fact table
+        '''
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         self.log.info("Implementing LoadFactOperator")
         fact_query = LoadFactOperator.sql_template.format(self.sql_query)
